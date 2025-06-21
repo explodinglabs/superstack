d9748fbfa2e173d5a7163c645714c80f81082ea8
Beau Barker <beau@explodinglabs.com>
2025-06-21 19:00:45 +1000

    Rename 00-setup-postgrest.sql to 00-init_postgrest.sql

--- postgres/migrations/00-init_postgrest.sql
+++ postgres/migrations/00-init_postgrest.sql
@@ -1,3 +1,5 @@
+begin;
+
 create role authenticator noinherit login password '${PGRST_AUTHENTICATOR_PASS}';
 create role anon;
 grant anon to authenticator;
@@ -5,3 +7,5 @@ create schema "${PGRST_DB_SCHEMAS}";
 comment on schema "${PGRST_DB_SCHEMAS}" is 'SuperStack API
 
 The api endpoints for the SuperStack application.';
+
+commit;
