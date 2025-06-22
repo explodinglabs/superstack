ec67317b9d0b25371defec9e8779d1499f0da66d
Beau Barker <beau@explodinglabs.com>
2025-06-23 09:33:27 +1000

    More informative comment on the api schema

--- postgres/migrations/00-init_postgrest.sql
+++ postgres/migrations/00-init_postgrest.sql
@@ -3,9 +3,12 @@ begin;
 create role authenticator noinherit login password '${PGRST_AUTHENTICATOR_PASS}';
 create role anon;
 grant anon to authenticator;
-create schema "${PGRST_DB_SCHEMAS}";
-comment on schema "${PGRST_DB_SCHEMAS}" is 'SuperStack API
+create schema api;
+comment on schema api is 'SuperStack API
 
-The api endpoints for the SuperStack application.';
+Main API schema for the SuperStack application. Contains all database
+functions, views, and tables exposed through PostgREST. Designed for use as a
+REST API interface, with endpoints documented via Swagger UI. This schema
+defines the public contract between the database and clients.';
 
 commit;
